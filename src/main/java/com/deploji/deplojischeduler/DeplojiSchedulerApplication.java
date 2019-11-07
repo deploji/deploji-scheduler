cae7dcb447f3835fa9137bd914f1016fa68f5c2a
Robert Sotomski <sotomski@gmail.com>
2019-11-07 10:16:33 +0100

    add empty project

---
+++ src/main/java/com/deploji/deplojischeduler/DeplojiSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.deploji.deplojischeduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeplojiSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeplojiSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deploji/deplojischeduler/DeplojiSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploji.deplojischeduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeplojiSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
